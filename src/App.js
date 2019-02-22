2a049a839a2b77c7d8c004ae389055bae10ffca5
Emmanuel <emmanuel@Emmanuels-MacBook-Pro.local>
2019-02-22 10:30:34 +0800

    validation and removing item added

--- src/App.js
+++ src/App.js
@@ -3,27 +3,70 @@ import './App.css';
 
 class App extends Component {
   state = {
-    buyItems:['Milk', 'Soap', 'Gari']
+    buyItems:['Milk', 'Soap', 'Gari'],
+    message: ''
     
   }
 
+  //Add new items
   addNewItem = (e) => {
    e.preventDefault()
    
-   const {buyItems} = this.state;
+   const {buyItems, message} = this.state;
    const newItem = this.newItem.value;
-   this.setState({
-    buyItems: [...this.state.buyItems, newItem]
-   })
+   
+   const isOnTheList = buyItems.includes(newItem)
+
+   if(isOnTheList){
+    this.setState({
+      message: 'This item already exist',
+      
+    })
+   }else{
+    newItem !=='' && this.setState({
+      buyItems: [...this.state.buyItems, newItem],
+      message: ''
+     })
+    
+   }
+   //Reseting input field
      this.addForm.reset()
 }
 
+    //Deleting /removing
+    removeItem = (items)=>{
+      const newItems = this.state.buyItems.filter((buyItem) =>{
+        return buyItem !== items;
+      })
+
+      this.setState({
+        buyItems: newItems
+      })
+      console.log('Remove', items)
+    }
+
+    display =() => {
+        return (
+         <div className="alert alert-danger " role="alert">
+            <h4 className="alert-heading">Error</h4>
+            <p>{this.state.message}</p>
+         </div>
+
+        );
+    }
+
   render() { 
-    const {buyItems} = this.state
+    //destructuring
+    const {buyItems, message} = this.state
+
+ 
     return (
       <div className="App">
         <h1 className='text-danger'>SHOPPING CART</h1>
-
+        <h3>Your total items is {buyItems.length}</h3>
+           {
+             message !=='' && this.display()
+           }
         <form onSubmit={this.addNewItem} ref={(input)=>{this.addForm =input}}>
            <div className='form-group'>
              <label className='sr-only'>Add New Item</label>
@@ -35,32 +78,28 @@ class App extends Component {
 
    <table className="table">
   <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">ITEM</th>
-      <th scope="col">ACTION</th>
-     
-    </tr>
-  </thead>
-  <tbody>
-  {
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">ITEM</th>
+        <th scope="col">ACTION</th>
+      
+      </tr>
+    </thead>
+    <tbody>
+        {
            buyItems.map((item) => {
-             return (
-              <tr>
-              <th scope="row">1</th>
-              <td>{item}</td>
-              <td><button className='btn btn-primary btn-sm'>Remove</button></td>
-            </tr>
-             );
-           })
-         }
-    
-   
-  </tbody>
-</table>
-        
-        
-      </div>
+                  return (
+                    <tr>
+                    <th scope="row">1</th>
+                    <td>{item}</td>
+                    <td><button type= 'button' onClick={() => this.removeItem(item)} className='btn btn-default btn-sm'>Remove</button></td>
+                  </tr>
+                  );
+                })
+              }
+     </tbody>
+   </table>
+  </div>
     );
   }
 }
